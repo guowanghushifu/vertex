8ac19d9c4528cf815bdb504d27e708440818ee68
guowanghushifu <80014670+guowanghushifu@users.noreply.github.com>
2025-06-25 21:33:08 +0800

    modify the client sort rule

--- app/common/Rss.js
+++ app/common/Rss.js
@@ -431,9 +431,22 @@ class Rss {
             (!item.maxLeechNum || item.maxLeechNum > item.maindata.leechingCount) &&
             (!item.minFreeSpace || item.minFreeSpace < item.maindata.freeSpaceOnDisk);
         })
-        .sort((a, b) => (this.clientSortBy === 'freeSpaceOnDisk' ? -1 : 1) *
-          (a.maindata[this.clientSortBy] - b.maindata[this.clientSortBy])
-        )[0] || availableClients[0];
+        .sort((a, b) => {
+          // 如果 this.clientSortBy 等于 freeSpaceOnDisk，只需要排序一次
+          if (this.clientSortBy === 'freeSpaceOnDisk') {
+            return b.maindata.freeSpaceOnDisk - a.maindata.freeSpaceOnDisk;
+          }
+          
+          // 否则先按照 this.clientSortBy 排序
+          const primarySort = a.maindata[this.clientSortBy] - b.maindata[this.clientSortBy];
+          
+          // 如果第一个排序条件相等，则按照 freeSpaceOnDisk 排序（降序，剩余空间大的优先）
+          if (primarySort === 0) {
+            return b.maindata.freeSpaceOnDisk - a.maindata.freeSpaceOnDisk;
+          }
+          
+          return primarySort;
+        })[0] || availableClients[0];
       const sqlRes = await util.getRecord('SELECT * FROM torrents WHERE hash = ? AND rss_id = ?', [torrent.hash, this.id]);
       if (sqlRes && sqlRes.id) continue;
       if (torrent.name.indexOf('[FROZEN]') !== -1) continue;
